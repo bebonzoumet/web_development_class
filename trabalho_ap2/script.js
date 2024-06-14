a1fab0cbcf5a1ce959bc7260da83726a61105f70
Bernardo James <bebonzoumet@hotmail.com>
2024-06-14 18:03:27 -0300

    ap2

---
+++ trabalho_ap2/script.js
@@ -0,0 +1,19 @@
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+
+
+
+
+// divTituloSenha.innerHTML = `
+// <div>
+// <h1>Atletas Botafogo 2024-1</h1>
+// <div>
+//     <label>
+//         Senha:
+//         <input type="password" id="senha">
+//     </label>
+//     <button id="btn_login">Entrar</button>
+// </div>
+// </div>
+// `
